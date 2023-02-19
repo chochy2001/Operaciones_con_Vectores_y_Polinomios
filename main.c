02354ffeb7fe1e218488ccd726636b3bc91057f6
Jorge Salgado Miranda <ohchochy@gmail.com>
2023-02-19 14:44:32 -0600

    Se hizo un ciclo hasta que el usuario decida terminar que se termine

--- main.c
+++ main.c
@@ -4,15 +4,19 @@
 int main() {
     int option;
     char input[10];
+    do {
     printf("Programa que realiza operaciones con vectores y polinomios\n");
     printf("Selecciona el tipo de operacion que quieres realizar\n");
-    printf("1) Vectores\n2) Polinomios\n");
+    printf("0) Salir\n1) Vectores\n2) Polinomios\n");
     if(fgets(input,10,stdin)==NULL){
         printf("Error: No se pudo leer la entrada.\n");
         exit(1);
     }
     option=(int)strtol(input, NULL, 10 );
     switch (option) {
+        case 0:
+            printf("*****Seleccionaste Salir*****\n");
+            break;
         case 1:
             printf("*****Seleccionaste Vectores*****\n");
             vectors();
@@ -25,6 +29,8 @@ int main() {
             printf("Opcion no valida\n");
             break;
     }
+
+    } while (option != 0);
 
     return 0;
 }
