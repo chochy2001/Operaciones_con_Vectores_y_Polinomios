9b9d91f46c2e4c91035d0edd95e2a071d4fbc1e6
Jorge Salgado Miranda <ohchochy@gmail.com>
2023-03-07 11:05:15 -0600

    Se comenzo con la creacion del proyecto

--- Encriptacion_2/main.cpp
+++ Encriptacion_2/main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int clave,opcion;
+    std::cout << "Programa que cifra y descifra una frase" << std::endl;
+    std::cout << "Ingresa la clave de cifrado (Tiene que ser un numero entero)" << std::endl;
+    std::cin>> clave;
+    std::cout << "La clave ingresada es " <<clave<< std::endl;
+    std::cout << "Que desea hacer\n1) Desencriptar\n2) Encriptar" << std::endl;
+    std::cin>>opcion;
+    switch (opcion) {
+        case 1:
+            std::cout << "La opcion deseada es Desencriptar" << std::endl;
+            break;
+        case 2:
+            std::cout << "La opcion deseada es Encriptar" << std::endl;
+            break;
+        default:
+            std::cout << "Opcion no valida" << std::endl;
+            break;
+    }
     return 0;
 }
---
+++ Encriptacion_2/tabla.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ohcho on 3/7/2023.
+//
+
+#include "tabla.hpp"
